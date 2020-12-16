83ba06737eddf3dd889d50e193406a7b4f987cd4
Italo <italocarvalhoti@hotmail.com>
2020-12-16 18:17:20 -0300

    fix style comments

---
+++ blog/static/js/blog.js
@@ -0,0 +1,19 @@
+//Comentario resizer
+jQuery.fn.extend({
+   autoHeight: function () {
+     function autoHeight_(element) {
+       return jQuery(element)
+         .css({ 'height': 'auto', 'overflow-y': 'hidden' })
+         .height(element.scrollHeight);
+     }
+     return this.each(function() {
+       autoHeight_(this).on('input', function() {
+         autoHeight_(this);
+       });
+     });
+   }
+ });
+$('textarea').autoHeight()
+//================
+
+
