e1964a433894f771fed345d2519d5ec6be8c45d3
Italo <italocarvalhoti@hotmail.com>
2020-12-17 16:43:50 -0300

    comment in post

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,16 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", {"post": post})
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/post_detail.html", {"post": post, "form": form})
 
 
 @login_required
@@ -79,20 +88,6 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect("post_list")
-
-
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect("post_detail", pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, "blog/add_comment_to_post.html", {"form": form})
 
 
 @login_required
